14a98df44844f09c807ecfe52101c2296008d9fc
RelebohileM <163429414+RelebohileM@users.noreply.github.com>
2024-04-26 02:04:47 -0700

    Add files via upload

---
+++ src/java/com/ajax/ComposerData.java
@@ -0,0 +1,77 @@
+package com.ajax;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author nbuser
+ */
+public class ComposerData {
+    
+    private HashMap composers = new HashMap();
+    
+
+    public HashMap getComposers() {
+        return composers;
+    }
+    
+    public ComposerData() {
+        
+        composers.put("1", new Composer("1", "Leseli", "Pitsa", "Baroque"));
+        composers.put("2", new Composer("2", "Lerato", "Sefuthi", "Baroque"));
+        composers.put("3", new Composer("3", "Lethabo", "Roma", "Baroque"));
+        composers.put("4", new Composer("4", "Letuma", "Phuthing", "Baroque"));
+        composers.put("5", new Composer("5", "Letetsoe", "Rome", "Baroque"));
+        composers.put("6", new Composer("6", "Lemphane", "Sefika", "Baroque"));
+        composers.put("7", new Composer("7", "Lethole", "Senkoa", "Baroque"));
+        
+        composers.put("8", new Composer("8", "Bokang", "Bereng", "Classical"));
+        composers.put("9", new Composer("9", "Bereng", "Bombere", "Classical"));
+        composers.put("10", new Composer("10", "Boitumelo", "Xaba", "Classical"));
+        composers.put("11", new Composer("11", "Boatile", "Jobile", "Classical"));
+        composers.put("12", new Composer("12", "Bohlale", "Bora", "Classical"));
+        composers.put("13", new Composer("13", "Boithatelo", "hararo", "Classical"));
+        composers.put("14", new Composer("14", "Boiki", "Mahlehle", "Classical"));
+        composers.put("15", new Composer("15", "Botho ", "Mozala", "Classical"));
+        composers.put("16", new Composer("16", "Baruting", "Pabalo", "Classical"));
+        composers.put("17", new Composer("17", "Babatle", "Rosalia", "Classical"));
+        composers.put("18", new Composer("18", "Botle", "Sello", "Classical"));
+        composers.put("19", new Composer("19", "Botleng", "Moloiloa", "Classical"));
+        
+        composers.put("20", new Composer("20", "Rethabile", "Makhele", "Romantic"));
+        composers.put("21", new Composer("21", "Relebohile", "Mohlekoa", "Romantic"));
+        composers.put("22", new Composer("22", "Relatile", "Mohobane", "Romantic"));
+        composers.put("23", new Composer("23", "Thabo", "Mohapi", "Romantic"));
+        composers.put("24", new Composer("24", "Thabisi", "Mapo", "Romantic"));
+        composers.put("25", new Composer("25", "Thabelo", "Lesenyeho", "Romantic"));
+        composers.put("26", new Composer("26", "Thabile", "Mapitsa", "Romantic"));
+        composers.put("27", new Composer("27", "Thoriso", "Maruping", "Romantic"));
+        composers.put("28", new Composer("28", "Thabo", "Khalla", "Romantic"));
+        composers.put("29", new Composer("29", "Thabong", "Thabong", "Romantic"));
+                
+        composers.put("30", new Composer("30", "Maleseli", "Pitsa", "Romantic"));
+        composers.put("31", new Composer("31", "Manthabiseng", "mohlekwa", "Romantic"));
+        composers.put("32", new Composer("32", "Maoratiloe", "Motoena", "Romantic"));
+        composers.put("33", new Composer("33", "Makarabelo", "Kori", "Romantic"));
+        composers.put("34", new Composer("34", "Matlhokomelo", "Tsiu", "Romantic"));
+        composers.put("35", new Composer("35", "Mapontso", "Mahaheng", "Romantic"));
+        composers.put("36", new Composer("36", "Makhalalelo", "Tsoeu", "Romantic"));
+        composers.put("37", new Composer("37", "Maitumeleng", "Sematle", "Romantic"));
+        composers.put("38", new Composer("38", "Maboitumelo", "Setimela", "Romantic"));
+        composers.put("39", new Composer("39", "Masenate", "Tau", "Romantic"));
+        composers.put("40", new Composer("40", "Mathabo", "Tladi", "Romantic"));
+        
+        composers.put("41", new Composer("41", "Sebela", "Batlokoa", "Post-Romantic"));
+        composers.put("42", new Composer("42", "Seema", "Tshiu", "Post-Romantic"));
+        composers.put("43", new Composer("43", "Seema", "Seema", "Post-Romantic"));
+        composers.put("44", new Composer("44", "Sello", "Poli", "Post-Romantic"));
+        composers.put("45", new Composer("45", "Senate", "Molapo", "Post-Romantic"));
+        composers.put("46", new Composer("46", "Soso", "Kabelo", "Post-Romantic"));
+        composers.put("47", new Composer("47", "Setumo", "Potula", "Post-Romantic"));
+        composers.put("48", new Composer("48", "Seratuoa", "Rafoneke", "Post-Romantic"));
+        composers.put("49", new Composer("49", "Seithati", "Stona", "Post-Romantic"));
+        composers.put("50", new Composer("50", "Semphetenyane", "Tefo", "Post-Romantic")); 
+        
+    }
+
+}
